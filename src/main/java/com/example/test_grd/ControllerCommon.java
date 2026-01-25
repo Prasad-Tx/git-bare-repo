de4094b22034b15c9e3c327539ba2c428e3f1c26
Prasad-Tx <prasad.thotagamuwa@osos.om>
2026-01-25 07:54:39 +0530

    refact(controller) : refactored common to remove org specific

--- src/main/java/com/example/test_grd/ControllerCommon.java
+++ src/main/java/com/example/test_grd/ControllerCommon.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ControllerCommon {
 
-    @GetMapping("org-a/gen-number")
+    @GetMapping("org/gen-number")
     public String hiGetNumber() {
         int random = (int) (Math.random() * 100);
         return "Number : " + random;
+    }
+
+    @GetMapping("org/gen-string")
+    public String hiGetString() {
+        int random = (int) (Math.random() * 100);
+        return "Number as string : " + random;
     }
 }
