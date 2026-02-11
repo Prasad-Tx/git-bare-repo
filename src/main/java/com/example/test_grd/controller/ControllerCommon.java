136869aba4daed70cd8a9f8f7382ce728117d76f
Prasad-Tx <prasad.thotagamuwa@osos.om>
2026-02-11 13:03:39 +0530

    feat(guard service) : implemented. | refactor(ControllerCommon.java) : moved into controller package

--- src/main/java/com/example/test_grd/controller/ControllerCommon.java
+++ src/main/java/com/example/test_grd/controller/ControllerCommon.java
@@ -1,10 +1,15 @@
-package com.example.test_grd;
+package com.example.test_grd.controller;
 
+import com.example.test_grd.service.GuardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerCommon {
+
+    @Autowired
+    GuardService guardService;
 
     @GetMapping("org/gen-number")
     public String hiGetNumber() {
@@ -16,5 +21,10 @@ public class ControllerCommon {
     public String hiGetString() {
         int random = (int) (Math.random() * 100);
         return "Number as string : " + random;
+    }
+
+    @GetMapping("org/guard")
+    public String guard() {
+        return guardService.guard();
     }
 }
