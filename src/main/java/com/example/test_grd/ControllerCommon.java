7e47d20d4857adc812071c9819972dec5db2a702
Prasad-Tx <prasad.thotagamuwa@osos.om>
2026-02-11 15:23:50 +0530

    feat(EnhanceService) : implementation and added endpoint

--- src/main/java/com/example/test_grd/ControllerCommon.java
+++ src/main/java/com/example/test_grd/ControllerCommon.java
@@ -1,10 +1,15 @@
 package com.example.test_grd;
 
+import com.example.test_grd.service.EnhanceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerCommon {
+
+    @Autowired
+    private EnhanceService enhanceService;
 
     @GetMapping("org/gen-number")
     public String hiGetNumber() {
@@ -16,5 +21,10 @@ public class ControllerCommon {
     public String hiGetString() {
         int random = (int) (Math.random() * 100);
         return "Number as string : " + random;
+    }
+
+    @GetMapping("org/hi")
+    public String hi() {
+        return enhanceService.hi();
     }
 }
