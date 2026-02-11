23be00852a3528792c0234049dcf5d2a27bcc028
Prasad-Tx <prasad.thotagamuwa@osos.om>
2026-02-11 13:11:21 +0530

    feat(NumberService) : implementation and added add endpoint

--- src/main/java/com/example/test_grd/controller/ControllerCommon.java
+++ src/main/java/com/example/test_grd/controller/ControllerCommon.java
@@ -2,11 +2,16 @@ package com.example.test_grd.controller;
 
 import com.example.test_grd.service.GuardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.example.test_grd.service.NumberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ControllerCommon {
+
+    @Autowired
+    NumberService numberService;
 
     @Autowired
     GuardService guardService;
@@ -21,6 +26,11 @@ public class ControllerCommon {
     public String hiGetString() {
         int random = (int) (Math.random() * 100);
         return "Number as string : " + random;
+    }
+
+    @GetMapping("org/add")
+    public String add() {
+        return String.valueOf(numberService.add(1, 2));
     }
 
     @GetMapping("org/guard")
